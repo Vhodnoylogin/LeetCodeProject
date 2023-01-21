9a4d3cee2355b4d196b0db024599830fca8209c3
Vhodnoylogin <kill.spama@gmail.com>
2023-01-21 20:24:35 +0300

    ы

--- leetcode/1443. Minimum Time to Collect All Apples in a Tree.py
+++ leetcode/1443. Minimum Time to Collect All Apples in a Tree.py
@@ -28,39 +28,60 @@ class Solution_old(ISolution):
 
 class Solution:
     def minTime(self, n: int, edges: List[List[int]], hasApple: List[bool]) -> int:
-        pass
+        if not edges:
+            return 0
+
+        tree = {}
+        for x in edges:
+            tree[x[0]] = []
+            tree[x[1]] = []
+        for x in edges:
+            tree[x[0]].append(x[1])
+            tree[x[1]].append(x[0])
+
+        # print(tree)
+        # print({i: k for i, k in enumerate(hasApple)})
+        return 2*sum(self._pass_tree(tree, 0, i, hasApple) for i in tree[0])
 
     def _pass_tree(self, tree: dict, node_from: int, node_current: int, has_apple: List[bool]) -> int:
-        p_tree = tree[node_current]
-        if node_current not in tree:
-            return 1 if has_apple[node_current] else 0
-        summ = sum(self._pass_tree(tree, i, has_apple) for i in tree[n] if i == node_from)
-        summ += 1 if has_apple[node_current] else 0
+        p_tree = [x for x in tree[node_current] if x != node_from]
+        summ = sum(self._pass_tree(tree, node_current, i, has_apple) for i in p_tree)
+        if summ > 0:
+            return summ + 1
+        return 1 if has_apple[node_current] else 0
+
 
 if __name__ == '__main__':
     n = 7
 
-    edges = [[0,1],[0,2],[1,4],[1,5],[2,3],[2,6]]
-    hasApple = [False, False, True, False, True, True, False]
+    # edges = [[0,1],[0,2],[1,4],[1,5],[2,3],[2,6]]
+    # hasApple = [False, False, True, False, True, True, False]
+    # r = Solution().minTime(n, edges, hasApple)
+    # print(r)
+    #
+    # edges = [[0,1],[0,2],[1,4],[1,5],[2,3],[2,6]]
+    # hasApple = [False, False, True, False, False, True, False]
+    # r = Solution().minTime(n, edges, hasApple)
+    # print(r)
+    #
+    # edges = [[0,1],[0,2],[1,4],[1,5],[2,3],[2,6]]
+    # hasApple = [False, False, False, False, False, False, False]
+    # r = Solution().minTime(n, edges, hasApple)
+    # print(r)
+    #
+    # edges = [[0,2],[0,3],[1,2]]
+    # hasApple = [False, True, False, False]
+    # r = Solution().minTime(n, edges, hasApple)
+    # print(r)
+    #
+    # edges = [[0,3],[1,2]]
+    # hasApple = [False, False, True, False]
+    # r = Solution().minTime(n, edges, hasApple)
+    # print(r)
+
+    n = 1
+    edges = []
+    hasApple = [True]
     r = Solution().minTime(n, edges, hasApple)
     print(r)
 
-    edges = [[0,1],[0,2],[1,4],[1,5],[2,3],[2,6]]
-    hasApple = [False, False, True, False, False, True, False]
-    r = Solution().minTime(n, edges, hasApple)
-    print(r)
-
-    edges = [[0,1],[0,2],[1,4],[1,5],[2,3],[2,6]]
-    hasApple = [False, False, False, False, False, False, False]
-    r = Solution().minTime(n, edges, hasApple)
-    print(r)
-
-    edges = [[0,2],[0,3],[1,2]]
-    hasApple = [False, True, False, False]
-    r = Solution().minTime(n, edges, hasApple)
-    print(r)
-
-    edges = [[0,3],[1,2]]
-    hasApple = [False, False, True, False]
-    r = Solution().minTime(n, edges, hasApple)
-    print(r)
